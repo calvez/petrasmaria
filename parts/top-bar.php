6bd84bce2fb03d79a84cd4c83e0ec49e4e52dc71
calvez <nemeth.lorant@gmail.com>
2015-06-05 17:45:22 +0200

    header image fent van

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -1,13 +1,11 @@
 <div class="top-bar-container contain-to-grid show-for-medium-up">
     <nav class="top-bar" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-            </li>
-        </ul>
-        <section class="top-bar-section">
+        <section class="top-bar-section ">
             <?php foundationpress_top_bar_l(); ?>
             <?php foundationpress_top_bar_r(); ?>
         </section>
     </nav>
+</div>
+<div class="row">
+  <div class="small-6 small-centered columns"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/header.png"></div>
 </div>
